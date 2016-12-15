06f9f91a350c68abd1a947720765880ac56a98ef
Ismail A <ieabbas@users.noreply.github.com>
2016-12-14 22:12:03 -0800

    Create Game.java
    - Consider changing series to 20 games

---
+++ Game.java
@@ -0,0 +1,15 @@
+package rpsGame;
+
+/*
+ * This program represents a simple game of "Rock, Paper, Scissors". The goal of the computer
+ * is to learn from the player's previous moves to determine what the next move will be, and 
+ * counter it. To do this, the computer will utilize one of many algorithms available to it.
+ * The player has 10 matches to win the series.
+ *
+ */
+
+public Class rpsGame {
+    public static void main (String[]args) {
+
+    }
+}
