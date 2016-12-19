b8042d79d2892f27af07aece5a89e823f55a8022
Ismail A <ieabbas@users.noreply.github.com>
2016-12-19 00:03:50 -0800

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,11 +1,21 @@
-package rpsGame;
+package roShamBo;
+import java.util.*;
 
 /*
- * This class will represent the computer that the player will face against.
- */
-public Class Computer {
-
-    public Computer{
-
-    }
+  This class will represent the computer that the player will face against. 
+*/
+public class Computer {
+    
+    /*
+      This field will be used to hold what the previous move by the player was.
+    */
+    private int lastMove;
+    private ArrayList gameHistory;
+    private ArrayList moveHistory;
+    
+    
+        
+    
+    
+    
 }
