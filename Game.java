8ccab2f4aad57c050ba4e5e18216cf36c5cf619e
Ismail A <ieabbas@users.noreply.github.com>
2016-12-18 22:54:57 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
-package rpsGame;
-
+package roShamBo;
+//import java.io.*;
+import java.util.*;
 /*
  * This program represents a simple game of "Rock, Paper, Scissors". The goal of the computer
  * is to learn from the player's previous moves to determine what the next move will be, and 
@@ -8,8 +9,11 @@ package rpsGame;
  *
  */
 
-public Class rpsGame {
+//1 = rock, 2 = paper, 3 = scissors
+public class Game {
     public static void main (String[]args) {
-
+        int numOfMatches;
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Hello World!");
     }
 }
